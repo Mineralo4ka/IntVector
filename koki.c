8c5aecd0c09794c20957a2a99433a90cf73f84b8
Mineralo4ka <andrew.pavlenko@mail.ru>
2017-03-02 17:19:55 +0700

    two commit

---
+++ koki.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    int N;
+
+    printf("Xotite repair all labs po vsem lessions?(y or n?)\nAnswer: ");
+    scanf("%c", &a);
+    printf("Ckolko tebe age?");
+    scanf("%d", &N);
+    
+    if (a == 'y'){
+	printf("You vibral right! Your labs bydyt cdelani :3(no eto ne tocho)");
+    }else{
+	printf("You are axyel tam? Ebanyi pidrila, poshla naxyu otcyda pcina!\n");
+	for (i = 0; i < N; i++){
+	    printf("%d\n", i);
+	}
+	    printf("Vot imenno stolko raz ti pidrila))00)00)0");
+    }
+
+    return 0;
+}
