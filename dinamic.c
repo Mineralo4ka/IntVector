a3934c327de70abde14ce3b1b0b90f38ec29f510
Mineralo4ka <andrew.pavlenko@mail.ru>
2017-03-09 21:15:42 +0700

    Add func size and capacity

--- dinamic.c
+++ dinamic.c
@@ -5,8 +5,8 @@
 typedef struct 
 {
 	int *v;
-	int size;
-	int capacity;
+	size_t size;
+	size_t capacity;
 
 } IntVector;
 
@@ -20,25 +20,53 @@ IntVector *int_vector_new(size_t initial_capacity)
 	return o;
 }
 
+size_t int_vector_get_size(const IntVector *v)
+{
+	return v -> size;
+}
+
+size_t int_vector_get_capacity(const IntVector *v)
+{
+	return v -> capacity;
+}
+
 IntVector *int_vector_copy(const IntVector *d)
 {
 	//Копирование массива
 	IntVector *o = malloc(sizeof(IntVector));
 	o -> v =  malloc(o -> capacity * sizeof(int));
-	memcpy(o -> v, d -> v, sizeof(int_vector_get_size(d -> size)));
-	o -> size = int_vector_get_size(d -> size);
-	o -> capacity = int_vector_get_capacity(d -> capacity);
+	memcpy(o -> v, d -> v, sizeof(int) * o -> size);
+	o -> size = int_vector_get_size(d);
+	o -> capacity = int_vector_get_capacity(d);
 	return o;
 }
 
+/*int int_vector_get_item(const IntVector *v, size_t index)
+{
+	return v -> v[index];
+}
+
+void int_vector_set_item(IntVector *v, size_t index, int item)
+{
+
+}*/
+
+void int_vector_free(IntVector *v)
+{
+	free(v);
+}
 
 int main()
 {
 	int i, N;
 	N = 6;
 
-	IntVector *w = int_vector_new(N);
-	IntVector *h = int_vector_copy(w);
+	IntVector *w, *h, *c;
+	w = int_vector_new(6);
+	h = int_vector_copy(w);
+	//c = int_vector_get_item(v, 2);
+	//int_vector_set_item(w, 2, 3);
+	int_vector_free(w);
 
 	printf("Созданный массив: \n");
 	for (i = 0; i < N; ++i) {
@@ -51,6 +79,8 @@ int main()
 		printf("%d", h -> v[i]);
 	}
 	printf("\n");
+
+	//printf("Вы получите второй элемент вектора: %d", c -> v);
 
 	return 0;
 }
